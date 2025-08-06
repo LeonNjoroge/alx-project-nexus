f51c6bc7c7efd6869788ad982b55e10f909d11f7
LeonNjoroge <leonmuraguri@gmail.com>
2025-08-06 19:39:36 +0300

    feat: Add navigation links

--- movie-app/src/components/Header.tsx
+++ movie-app/src/components/Header.tsx
@@ -1,4 +1,11 @@
 export default function Header() {
+    const navLinks = [
+        { name: "Home", href: "/" },
+        { name: "Search", href: "/search" },
+        { name: "Saved", href: "/saved" },
+        { name: "Profile", href: "/profile" },
+    ];
+
  return(
      <header>
         Header component
