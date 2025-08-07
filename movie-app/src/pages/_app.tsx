3a583f44dbf3915fd24d7af88de5fab458730b09
LeonNjoroge <leonmuraguri@gmail.com>
2025-08-07 14:20:00 +0300

    feat: add padding to site

--- movie-app/src/pages/_app.tsx
+++ movie-app/src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
     <Header />
-    <main className="min-h-screen bg-[var(--app-bg)] text-[var(--app-fg)]">
+    <main className="min-h-screen bg-[var(--app-bg)] text-[var(--app-fg)] p-10">
       <Component {...pageProps} />
     </main>
     </>
