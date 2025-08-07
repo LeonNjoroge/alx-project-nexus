a11ccdecc4231515e2b84e35c753671f9e91d794
LeonNjoroge <leonmuraguri@gmail.com>
2025-08-07 19:21:31 +0300

    feat: add session to app

--- movie-app/src/pages/_app.tsx
+++ movie-app/src/pages/_app.tsx
@@ -1,15 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-  <>
-    <Header />
-    <main className="min-h-screen bg-[var(--app-bg)] text-[var(--app-fg)] p-10">
-      <Component {...pageProps} />
-    </main>
-    </>
-);
-
+      <SessionProvider session={session}>
+        <Header />
+        <main className="min-h-screen bg-[var(--app-bg)] text-[var(--app-fg)] p-10">
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+  );
 }
