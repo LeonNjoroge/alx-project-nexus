5de2f3be897b9230e0ba71ce4331ccd30e18ad4f
LeonNjoroge <leonmuraguri@gmail.com>
2025-08-09 15:21:20 +0300

    feat: pass session props to home page

--- movie-app/src/pages/_app.tsx
+++ movie-app/src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "@/components/Header";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import Header from "@/components/Header";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-      <SessionProvider session={session}>
-        <Header />
-        <main className="min-h-screen bg-[var(--app-bg)] text-[var(--app-fg)] p-10">
-          <Component {...pageProps} />
-        </main>
-      </SessionProvider>
-  );
+export default function App({
+                                Component,
+                                pageProps: { session, ...pageProps },
+                            }: AppProps<{ session: Session | null }>) {
+    return (
+        <SessionProvider session={session}>
+            <Header />
+            <main className="min-h-screen bg-[var(--app-bg)] text-[var(--app-fg)] p-10">
+                <Component {...pageProps} />
+            </main>
+        </SessionProvider>
+    );
 }
