22397351ba03af391d354c2c3e5e7cfdeb82f675
LeonNjoroge <leonmuraguri@gmail.com>
2025-08-09 15:20:34 +0300

    feat: add next authentication type

---
+++ movie-app/src/types/next-auth.d.ts
@@ -0,0 +1,23 @@
+// types/next-auth.d.ts
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+    interface Session {
+        user?: DefaultSession["user"] & {
+            id: string; // we attach this in session callback
+        };
+    }
+
+    interface User {
+        id: string;
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id?: string;
+    }
+}
